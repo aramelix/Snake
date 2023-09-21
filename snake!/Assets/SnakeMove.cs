778973084011334b0b58d7c743d6a9474cbd46a9
aramelix <144147993+aramelix@users.noreply.github.com>
2023-09-21 15:06:55 -0400

    Says that "Input" is wrong, talk to teacher about it next week

--- snake!/Assets/SnakeMove.cs
+++ snake!/Assets/SnakeMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class SnakeMove : MonoBehaviour
 {
@@ -17,9 +18,30 @@ public class SnakeMove : MonoBehaviour
     void Update()
     {
         //change direction of the snake 
-        if (Input .GetKeyDown(KeyCode.W)) //When W key is pressed... 
+        if (Input.GetKeyDown(KeyCode.W)) //When W key is pressed... 
         {
             direction = Vector2.up; //go up 
         }
+        else if (Input.GetKeyDown(KeyCode.A)) //When A key is pressed... 
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) //When S key is pressed...
+        {
+            direction = Vector2.down; //go down 
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) //When D key is pressed... 
+        {
+            direction = Vector2.right; //go right 
+        }
+    }
+
+    //FixedUpdate is called at a fixed interval. 
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector2( //get the position 
+            Mathf.Round(this.transform.position.x) + direction.x, //round the number add value to X 
+            Mathf.Round(this.transform.position.y) + direction.y  //round the number add value to Y 
+            ); 
     }
 }
